a65424931f3ff9895a5c58166ee8c8c78f924553
Charalampos Kontis <kontis.char@gmail.com>
2026-01-21 21:01:59 +0200

    Last v1_1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/football-standings/"
+  // base: "football-standings", // remove this line for Vercel
 });
